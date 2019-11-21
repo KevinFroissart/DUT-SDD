4b32ba7e618940fb763920f0163cc5c6449425f2
Froissart Kévin <kevin.froissart69@gmail.com>
2019-11-21 16:56:49 +0100

    ctp

---
+++ src/tp06/FileDePrioriteReductionException.java
@@ -0,0 +1,18 @@
+package ssd_ctp;
+
+public class FileDePrioriteReductionException extends Throwable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FileDePrioriteReductionException() {
+		super();
+	}
+
+	public FileDePrioriteReductionException(String message) {
+		super(message);
+	}
+
+}
